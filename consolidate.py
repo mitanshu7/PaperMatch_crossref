deac1032c8e0c1285ffa2f40b1c067b9f8e2b1c4
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-20 15:39:44 +0530

    Merge all parquet files into one, optionally also create shards if needed

---
+++ consolidate.py
@@ -0,0 +1,27 @@
+# Import required libraries
+import os
+
+from datasets import load_dataset
+
+# Also, whether to split the consolidated file into parts
+# Set 0 for no sharding.
+num_parts = 0
+
+# Create a new dataset
+dataset = load_dataset("parquet", data_dir="crossref_metadata/", num_proc=24)
+
+# Save consolidated dataset
+dataset["train"].to_parquet("crossref_metadata.parquet")
+
+# Save consolidated dataset in parts
+if num_parts:
+    
+    print(f"Splitting consolidated dataset in {num_parts} parts.")
+    
+    split_folder = 'crossref_metadata_split'
+    os.makedirs(split_folder, exist_ok=True)
+
+    for i in range(num_parts):
+        dataset["train"].shard(num_parts, index=i).to_parquet(
+            f"{split_folder}/part_{i + 1}.parquet"
+        )
