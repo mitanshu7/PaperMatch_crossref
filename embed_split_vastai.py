3e614332fcf4cb5106c45e9305f515cf433eafab
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-22 10:47:58 +0530

    Embedding for vastai works on a single gpu setup, will check multi gpu next

--- embed_split_vastai.py
+++ embed_split_vastai.py
@@ -1,15 +1,13 @@
 # Import required libraries
 import os
-from glob import glob
-from multiprocessing import Pool, set_start_method
+from multiprocess import set_start_method
 
-import pandas as pd
 from sentence_transformers import SentenceTransformer
 import numpy as np
-from huggingface_hub import snapshot_download, HfApi
+from huggingface_hub import snapshot_download
 from dotenv import load_dotenv
 import torch
-import backoff
+from datasets import load_dataset
 
 ################################################################################
 
@@ -17,100 +15,90 @@ import backoff
 load_dotenv('.env')
 
 # Declare batch size
-BATCH_SIZE = os.getenv('BATCH_SIZE')
-print(BATCH_SIZE)
+BATCH_SIZE = int(os.getenv('BATCH_SIZE'))
+print(f'Using BATCH SIZE: {BATCH_SIZE}')
 
 # Define the embedding model
-model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
-
-# Folder to save files in
-embedding_folder = "crossref_embedding_split"
-os.makedirs(embedding_folder, exist_ok=True)
-
-embedding_folder_binary = "crossref_embedding_split_binary"
-os.makedirs(embedding_folder_binary, exist_ok=True)
-
-# Connect to hfapi
-hf_client = HfApi(token=os.getenv('HF_ACCESS_TOKEN'))
-
-# Get the HF repo name 
-destination_repo_name = os.getenv('HF_REPO_EMBEDDING_SPLIT')
-
-# Extract Hugging face username
-username = hf_client.whoami()['name']
-print(f"Uploading to {username}'s huggingface account!")
-
-# Create repos to uplod files to
-embedding_repo_id = f"{username}/{destination_repo_name}"
-hf_client.create_repo(embedding_repo_id, private=False, repo_type='dataset', exist_ok=True)
-
-binary_embedding_repo_id = f"{username}/{destination_repo_name + '_binary'}"
-hf_client.create_repo(binary_embedding_repo_id, private=False, repo_type='dataset', exist_ok=True)
-
+embedding_model_name = "mixedbread-ai/mxbai-embed-large-v1"
+print(f'Using embedding model: {embedding_model_name}')
+model = SentenceTransformer(embedding_model_name)
 
 ################################################################################
 
 # Download the split dataset
 metadata_repo_id = os.getenv('HF_REPO_METADATA_SPLIT')
+print(f'Download metadata from: {metadata_repo_id}')
 snapshot_download(repo_id=metadata_repo_id, repo_type='dataset', local_dir=metadata_repo_id)
 
-# Split metadata
-split_metadata_files = glob(f"{metadata_repo_id}/*.parquet")
-split_metadata_files.sort()
-
+# Repo names to upload data to. 
+# TODO create new repo here only
+embedding_repo_id = os.getenv('HF_REPO_EMBEDDING_SPLIT')
+embedding_repo_id_binary = os.getenv('HF_REPO_EMBEDDING_SPLIT_BINARY')
+print(f'Will upload float embeddings to {embedding_repo_id} and binary to {embedding_repo_id_binary}.')
 ################################################################################
 
-@backoff.on_exception(backoff.expo, torch.OutOfMemoryError, jitter=backoff.full_jitter)
-def process_metadata(device_number, metadata_file):
+def embed_metadata(batch, rank):
     
     # Move the model on the right GPU if it's not there already
-    device = f"cuda:{device_number % torch.cuda.device_count()}"
+    device = f"cuda:{(rank or 0) % torch.cuda.device_count()}"
+    print(f'Loading Embedding model to GPU:{device}')
     model.to(device)
-    
-    # Read
-    df = pd.read_parquet(metadata_file)
-    
-    # try
-    df = df.sample(BATCH_SIZE*10)
 
     # Calculate the embeddings for text and paragraphs
-    df["vector"] = model.encode(
-        df["abstract"].tolist(),
+    batch["vector"] = model.encode(
+        batch["abstract"],
         batch_size=BATCH_SIZE,
         show_progress_bar=True,
         convert_to_numpy=True,
-    ).tolist()
-    
-    # Convert lists back to numpy arrays with the correct dtype
-    df["vector"] = df["vector"].apply(
-        lambda x: np.array(x, dtype=np.float32)
     )
-
-    # Save Float
-    embed_file = f"{embedding_folder}/{os.path.basename(metadata_file)}"
-    df.to_parquet(embed_file, index=False)
     
-    # Upload Float
-    hf_client.upload_file(path_or_fileobj=embed_file, path_in_repo=embed_file, repo_id=embedding_repo_id, repo_type='dataset')
+    return batch
+
+# Function to binarise float embeddings
+def binarise(row):
+    # Make it a numpy array, since batching sends it as list
+    float_vector = np.array(row['vector'], dtype=np.float32)
     
     # Binarise
-    df['vector'] = df['vector'].apply(lambda dense_vector : np.packbits(np.where(dense_vector >= 0, 1, 0)).tobytes())
+    binary_vector = np.where(float_vector >= 0, 1, 0)
     
-    # Save Binary
-    binary_embed_file = f"{embedding_folder_binary}/{os.path.basename(metadata_file)}"
-    df.to_parquet(binary_embed_file, index=False)
+    # Pack it to make it milvus compatible
+    row['vector'] = np.packbits(binary_vector).tobytes()
     
-    # Upload Binary
-    hf_client.upload_file(path_or_fileobj=binary_embed_file, path_in_repo=binary_embed_file, repo_id=binary_embedding_repo_id, repo_type='dataset')
-
+    return row
+    
 ################################################################################
 
 if __name__ == "__main__":
-    torch.multiprocessing.set_start_method('spawn', force=True)
-
+    # Set context for GPU multiprocess
+    set_start_method('spawn')
+    
+    # Load dataset
+    print(f'Loading dataset')
+    dataset = load_dataset("parquet", data_dir=metadata_repo_id, split='train')
+    
     # try
-    split_metadata_files = split_metadata_files[:2]
+    print(f"Slicing dataset to try it out")
+    dataset = dataset.shuffle().take(BATCH_SIZE*20)
 
-
-    with Pool(torch.cuda.device_count()) as pool:
-        results = pool.starmap(process_metadata, enumerate(split_metadata_files))
+    # Embed
+    print('Embedding abstracts')
+    dataset = dataset.map(
+        embed_metadata,
+        batched=True,
+        batch_size=BATCH_SIZE,
+        with_rank=True,
+        num_proc=torch.cuda.device_count(),  # one process per GPU
+    )
+    
+    # Upload floats
+    print('Uploading float embeddings')
+    dataset.push_to_hub(embedding_repo_id)
+    
+    # Binarise embeddings
+    print('Binarising vectors')
+    dataset = dataset.map(binarise)
+    
+    # Upload binarised vectors
+    print('Uploading binary embeddings')
+    dataset.push_to_hub(embedding_repo_id_binary)
