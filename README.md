e547c23b082da8cd5078d499b2c9afb8530863fb
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-24 00:17:16 +0530

    Routine

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 When logging into vast.ai,
+pytorch 2.2 works with rtx 4090 on runpod. 
+
 update uv from website, 
+
 login to hf using cli
 add token to env
+Change batch size in env
 
-AFTER testing for batch size, remove the slicing to start full script
+AFTER testing for batch size, on sample dataset
+, allow_patterns='part_1.parquet'
+remove the slicing to start full script
 
+nohup uv run embed_multigpu_split.py > output.log 2>&1 &
--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import re
-from ast import literal_eval
-from bs4 import BeautifulSoup
-
-text = """<JATS1:p>This accessible and engaging text covering sketch, sitcom and comedy drama, alongside improvisation and stand-up, brings together a panoply of tools and techniques for creating short and long-form comedy narratives for live performance, TV and online.</JATS1:p>
-          <JATS1:p>Referencing a broad range of comedy from both sides of the Atlantic, spanning several decades and including material on contemporary internet sketches, it offers all kinds of useful advice on creating comic narratives for stage and screen: using life experience as raw material; constructing comedy worlds; creating comic characters, their relationships and interactions; structuring sketches, scenes and routines; and developing and plotting stories.</JATS1:p>
-          <JATS1:p>The book’s interviewees, from the UK and the USA, feature stand-ups, sketch comics, improvisers and TV comedy producers, and include Steve Kaplan, Hollywood comedy guru and author of The Hidden Tools of Comedy, Will Hines teacher and improviser from the Upright Citizens Brigade Theatre and Lucy Lumsden TV producer and former Controller of Comedy Commissioning for BBC.</JATS1:p>
-          <JATS1:p>Written by “the ideal person to nurture new talent” (The Guardian), Creating Comedy Narratives for Stage &amp; Screen includes material you won’t find anywhere else and is a stimulating resource for comedy students and their teachers, with a range and a depth that will be appreciated by even the most eclectic and multi-hyphenated writers and performers.</JATS1:p>"""
-
-# regex = re.compile(r"(?:<jats:p>)[\w\W]+(?:</jats:p>)")
-
-# print(regex.findall(text))
-
-soup = BeautifulSoup(text, 'html.parser')
-# print(soup.find('jats:p').get_text())
-
-print(soup.find('wtf').get_text())
-
-# from datetime import datetime
-
-# a = datetime.strptime('2017-10-17T01:10:23Z', 
-# print(int(a.strftime('%Y')))
-# print(a)
+# Import required libraries
+import os
+os.makedirs('tmp/tmp2/tmp3', exist_ok=True)
